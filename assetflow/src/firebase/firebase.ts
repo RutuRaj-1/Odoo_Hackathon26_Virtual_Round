0b068b5550ef380606d3b5adf15e265e01e1fa1e
RutuRaj-1 <bhomeruturaj@gmail.com>
2026-07-12 11:03:52 +0530

    API Key Secrets Updated

--- assetflow/src/firebase/firebase.ts
+++ assetflow/src/firebase/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0FEAv_lovvQW8Hle96nFdJkHEVgM61x0",
-  authDomain: "odoohackathon-virtualround.firebaseapp.com",
-  projectId: "odoohackathon-virtualround",
-  storageBucket: "odoohackathon-virtualround.firebasestorage.app",
-  messagingSenderId: "427265237132",
-  appId: "1:427265237132:web:f32ecced4a5f34917c1ba9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
